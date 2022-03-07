63d1176b69500f64af0f7bbd38635802c664c54e
Mark <mtstarbinski@outlook.com>
2022-03-06 23:06:38 -0500

    installed react router and set up appropriate routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
-import Homepage from './pages/Homepage';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Hats from "./components/Hats";
 
 function App() {
   return (
-    <Homepage/>
+    <Routes>
+      <Route path="/">
+        <Route index element={<Homepage />} />
+        <Route path="shop/">
+          <Route path="hats" element={<Hats />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
